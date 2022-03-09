893bfbe4531a66a6fabffff284f4721f9e3df40e
Jannessa <53408270+jmarkovich2@users.noreply.github.com>
2022-03-09 14:34:17 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+//jshint eversion:
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.get("/projects", function(req, res){
+  res.render("projects");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
